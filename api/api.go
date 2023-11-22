0f8756b9a9d2a0c3724d3fb90a31ff8ac4374b9a
Jack Rendor <12821606+jackrendor@users.noreply.github.com>
2023-11-22 15:12:51 +0100

    Added shodan and censys API

--- api/api.go
+++ api/api.go
@@ -2,10 +2,15 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/jackrendor/lazydnsfinder/config"
+	"github.com/tidwall/gjson"
 )
 
 var Client http.Client
@@ -37,6 +42,7 @@ func CRTSH(domainArg string) []string {
 		return nil
 	}
 
+	fmt.Println("[CRTSH] interrogating...")
 	resp, respErr := Client.Do(req)
 	if respErr != nil {
 		log.Println("[CRTSH]", respErr.Error())
@@ -67,6 +73,7 @@ func HackerTarget(domainArg string) []string {
 		log.Println("[HackerTarget]", reqErr.Error())
 		return nil
 	}
+	fmt.Println("[HackerTarget] interrogating...")
 	resp, respErr := Client.Do(req)
 	if respErr != nil {
 		log.Println("[HackerTarget]", respErr.Error())
@@ -74,7 +81,7 @@ func HackerTarget(domainArg string) []string {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Println("[HackerTarget]", respErr.Error())
+		log.Println("[HackerTarget]", resp.Status)
 		return nil
 	}
 	bodyBytes, readAllErr := io.ReadAll(resp.Body)
@@ -91,4 +98,200 @@ func HackerTarget(domainArg string) []string {
 
 	return result
 
+}
+
+func CensysCerts(domainArg string, next string, maxRecursion int) []string {
+	var result []string
+	if maxRecursion <= 0 {
+		return result
+	} else {
+		maxRecursion--
+	}
+	req, reqErr := http.NewRequest("GET", "https://search.censys.io/api/v2/certificates/search", nil)
+	if reqErr != nil {
+		log.Println("[Censys Certs]", reqErr.Error())
+		return result
+	}
+	req.SetBasicAuth(config.Values.Censys.APIID, config.Values.Censys.APISECRET)
+
+	query := req.URL.Query()
+
+	query.Set("q", domainArg)
+	query.Set("per_page", "100")
+	req.URL.RawQuery = query.Encode()
+
+	if next != "" {
+		query.Set("cursor", next)
+	}
+
+	fmt.Println("[Censys Certs] interrogating...")
+	resp, respErr := Client.Do(req)
+	if respErr != nil {
+		log.Println("[Censys Certs]", respErr.Error())
+		return result
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println("[Censys Certs]", resp.Status)
+		return result
+	}
+
+	bodyBytes, readAllErr := io.ReadAll(resp.Body)
+	if readAllErr != nil {
+		log.Println("[Censys Certs]", respErr.Error())
+		return result
+	}
+
+	parsedJson := gjson.ParseBytes(bodyBytes)
+	if parsedJson.Get("code").Num != 200 {
+		log.Println("[Censys Certs]", parsedJson.Get("error"))
+		return result
+	}
+
+	tmp := make(map[string]bool)
+	for _, elem := range parsedJson.Get("result.hits.#.names").Array() {
+		for _, nameCert := range elem.Array() {
+			if !strings.HasPrefix(nameCert.Str, "*.") {
+				tmp[nameCert.Str] = true
+			}
+		}
+
+	}
+
+	// removes duplicates
+	for key := range tmp {
+		result = append(result, key)
+	}
+
+	nextCursor := parsedJson.Get("links.next").Str
+	if nextCursor != "" {
+		newdata := CensysCerts(domainArg, nextCursor, maxRecursion)
+		result = append(result, newdata...)
+	}
+
+	return result
+}
+
+func CensysHosts(domainArg string, next string, maxRecursion int) []string {
+	var result []string
+	if maxRecursion <= 0 {
+		return result
+	} else {
+		maxRecursion--
+	}
+	req, reqErr := http.NewRequest("GET", "https://search.censys.io/api/v2/hosts/search", nil)
+	if reqErr != nil {
+		log.Println("[Censys Hosts]", reqErr.Error())
+		return result
+	}
+	req.SetBasicAuth(config.Values.Censys.APIID, config.Values.Censys.APISECRET)
+
+	query := req.URL.Query()
+	query.Set("q", domainArg)
+	query.Set("per_page", "100")
+	query.Set("virtual_hosts", "INCLUDE")
+	query.Set("sort", "RELEVANCE")
+	if next != "" {
+		query.Set("cursor", next)
+	}
+	req.URL.RawQuery = query.Encode()
+
+	fmt.Println("[Censys Hosts] interrogating...")
+	resp, respErr := Client.Do(req)
+	if respErr != nil {
+		log.Println("[Censys Hosts]", respErr.Error())
+		return result
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println("[Censys Hosts]", resp.Status)
+		return result
+	}
+
+	bodyBytes, readAllErr := io.ReadAll(resp.Body)
+	if readAllErr != nil {
+		log.Println("[Censys]", respErr.Error())
+		return result
+	}
+
+	parsedJson := gjson.ParseBytes(bodyBytes)
+	if parsedJson.Get("code").Num != 200 {
+		log.Println("[Censys Hosts]", parsedJson.Get("error"))
+		return result
+	}
+
+	tmp := make(map[string]bool)
+	for _, elem := range parsedJson.Get("result.hits.#.name").Array() {
+		tmp[elem.Str] = true
+
+	}
+	// removes duplicates
+	for key := range tmp {
+		result = append(result, key)
+	}
+
+	nextCursor := parsedJson.Get("links.next").Str
+	if nextCursor != "" {
+		newdata := CensysHosts(domainArg, nextCursor, maxRecursion)
+		result = append(result, newdata...)
+	}
+
+	return result
+}
+
+func Shodan(domainArg string, next int, maxRecursion int) []string {
+	var result []string
+	next++
+	if maxRecursion <= 0 {
+		return result
+	} else {
+		maxRecursion--
+	}
+	req, reqErr := http.NewRequest("GET", "https://api.shodan.io/dns/domain/"+domainArg, nil)
+	if reqErr != nil {
+		log.Println("[Shodan]", reqErr.Error())
+		return result
+	}
+
+	query := req.URL.Query()
+	query.Set("key", config.Values.Shodan.APIKEY)
+	query.Set("page", strconv.Itoa(next))
+
+	req.URL.RawQuery = query.Encode()
+
+	fmt.Println("[Shodan] interrogating...")
+	resp, respErr := Client.Do(req)
+	if respErr != nil {
+		log.Println("[Shodan]", respErr.Error())
+		return result
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println("[Shodan]", resp.Status)
+		return result
+	}
+
+	bodyBytes, readAllErr := io.ReadAll(resp.Body)
+	if readAllErr != nil {
+		log.Println("[Shodan]", respErr.Error())
+		return result
+	}
+
+	parsedJson := gjson.ParseBytes(bodyBytes)
+
+	tmp := make(map[string]bool)
+	for _, elem := range parsedJson.Get("subdomains").Array() {
+		tmp[elem.Str+"."+domainArg] = true
+
+	}
+	// removes duplicates
+	for key := range tmp {
+		result = append(result, key)
+	}
+
+	if parsedJson.Get("more").Bool() {
+		newdata := Shodan(domainArg, next, maxRecursion)
+		result = append(result, newdata...)
+	}
+	return result
 }
